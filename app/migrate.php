abdfed52b88f8ee73aca05e6fb632ad217925f72
guiszlima <gui.spicacci.dev@gmail.com>
2025-04-24 01:55:22 -0300

    Uncomment migration logic to enable database migrations execution

--- app/migrate.php
+++ app/migrate.php
@@ -1,16 +1,16 @@
 <?php
 
-// $pdo = new PDO('mysql:host=db;port=3306;dbname=app_db', 'user', 'secret');
+$pdo = new PDO('mysql:host=db;port=3306;dbname=app_db', 'user', 'secret');
 
-// $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// foreach (glob('migrations/*.php') as $file) {
-//     $migration = require $file;
+foreach (glob('migrations/*.php') as $file) {
+    $migration = require $file;
 
-//     if (is_callable($migration)) {
-//         echo "Executando: $file\n";
-//         $migration($pdo);
-//     } else {
-//         echo "⚠️ Arquivo $file não retornou uma função.\n";
-//     }
-// }
+    if (is_callable($migration)) {
+        echo "Executando: $file\n";
+        $migration($pdo);
+    } else {
+        echo "⚠️ Arquivo $file não retornou uma função.\n";
+    }
+}
